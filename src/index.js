cad4aced36c56016655e42be79753cb3d54ee1a1
DESKTOP-USG080I\barto <bartorex@gmail.com>
2017-05-05 17:08:45 +0200

    Fix for import Provider from react-redux

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
 import configureStore from './store/configureStore';
-import Provider from 'react-redux';
+import {Provider} from 'react-redux';
 
 const store = configureStore();
 
